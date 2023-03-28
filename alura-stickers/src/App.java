ce5b39ad6242b95db968fa1f4b6c626798da8a60
SousaPedrop <sousapedro.work@gmail.com>
2023-03-28 17:09:43 -0300

    usando classe geradora de figurinhas

--- alura-stickers/src/App.java
+++ alura-stickers/src/App.java
@@ -1,9 +1,12 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -32,13 +35,23 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         
         // exibir e manipular os dados da maneira que quisermos
+        String urlImagem;
+        String titulo;
+        var geradora = new GeradoraDeFigurinhas();
+
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("\u001B[31m\u001b[1mTítulo: \u001b[0m\u001b[3m\u001B[34m" + filme.get("title") + "\u001b[0m");
-            System.out.println("\u001b[1mImagem: \u001b[0m\u001b[4m" + filme.get("image") + "\u001b[0m");
-            System.out.println("\u001B[33m\u001b[1mNota: \u001b[3m" + filme.get("imDbRating") + "\u001b[0m");
-            for (int i = 0; i < Math.floor(Double.parseDouble(filme.get("imDbRating"))); i++) {
-                System.out.print("⭐");
-            }
+            titulo = filme.get("title") + ".png";
+            urlImagem = filme.get("image");
+            InputStream inputStream = new URL(urlImagem).openStream();
+
+            geradora.criar(inputStream, titulo);
+
+            System.out.println("\u001B[31m\u001b[1mTítulo: \u001b[0m\u001b[3m\u001B[34m" + titulo + "\u001b[0m");
+            // System.out.println("\u001b[1mImagem: \u001b[0m\u001b[4m" + urlImagem + "\u001b[0m");
+            // System.out.println("\u001B[33m\u001b[1mNota: \u001b[3m" + filme.get("imDbRating") + "\u001b[0m");
+            // for (int i = 0; i < Math.floor(Double.parseDouble(filme.get("imDbRating"))); i++) {
+            //     System.out.print("⭐");
+            // }
             System.out.println("\n===================================");
         }
     }//	\u001B[34m
